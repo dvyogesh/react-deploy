6f6be067554cf94d1dcfadd713df3ee919bef17f
Yogesh <yogesh@Yogeshs-MacBook-Air.local>
2020-03-02 18:21:21 +0530

    Updates

---
+++ precache-manifest.06137f7182c353c3402da69a54a17645.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4832bb96a89bf5728bd743545453f82",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "1cd6fcba14136bc27f5c",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0f57c089b253cb80f6c3",
+    "url": "/react-deploy/static/js/2.dfafb019.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-deploy/static/js/2.dfafb019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd6fcba14136bc27f5c",
+    "url": "/react-deploy/static/js/main.ca7c9bfb.chunk.js"
+  },
+  {
+    "revision": "7d906fe467758e37b141",
+    "url": "/react-deploy/static/js/runtime-main.c7451532.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
